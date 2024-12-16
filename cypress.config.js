852a7dcf327e58c756aa4b792a10d05d04a3c9df
Rahul Karmarkar <90101820+rahul-karmarkar@users.noreply.github.com>
2024-12-16 17:05:16 +0530

    test fitpartners page and Near by gym half functionality

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  "url": "https://fitzone.infominez.com",
+  "chromeWebSecurity": false,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        // If it's Chrome, launch it in incognito mode
+        if (browser.family === 'chrome') {
+          launchOptions.args.push('--incognito');
+        }
+        return launchOptions;
+      });
     },
     specPattern: 'cypress/integrations/examples/*.js'
   },
